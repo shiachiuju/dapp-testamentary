7dcd6bbf69be4761f6ca12f0ba26e5d76651ce7b
shiachiu <chiu338920@gmail.com>
2021-08-07 16:01:17 +0800

    Revert "1"
    This reverts commit 6180bfa67b59b21f3ade87eb3e0a4161ab1ad60f.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
